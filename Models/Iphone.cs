47bd2ebcd1e95c79822330ada36125205cb6edea
Yuri Rocha <yurirocha15nascimento@outlook.com>
2024-11-26 14:59:30 -0300

    Desafio concluido com sucessogit status

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    //FEITO
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //FEITO
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//FEITO
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "C20", imei: "111222333", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "13 Pro Max", imei: "111222333", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Discord");
